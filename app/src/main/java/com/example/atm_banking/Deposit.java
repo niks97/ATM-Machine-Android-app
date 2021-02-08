4d5c080b385c12863167e99ce67ac9c51664e294
Nikhil <nikhilsaindane53@gmail.com>
2021-02-08 12:28:10 +0530

    ATM_Banking

---
+++ app/src/main/java/com/example/atm_banking/Deposit.java
@@ -0,0 +1,27 @@
+package com.example.atm_banking;
+
+
+public class Deposit {
+
+    private double balance;
+    private double dpValue;
+    //set current balance
+    public void setBalance(double b) {
+
+        balance = b;
+
+    }//end setBalance
+
+    //set deposit value entered by user
+    public void setDeposit (double dp) {
+
+        dpValue = dp;
+
+    }//end setDeposit
+    //calculate and return new balance
+    public double getNewBalance() {
+
+        return balance + dpValue;
+
+    }// end getNewBalance
+}//end Deposit
